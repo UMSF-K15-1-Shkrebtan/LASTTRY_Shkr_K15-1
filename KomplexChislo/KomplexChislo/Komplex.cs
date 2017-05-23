03a7efdf3bacd2992dbafad0f7772a8bc4b9ebd2
UMSF-K15-1-Shkrebtan <magnusseniren@gmail.com>
2017-05-23 20:50:10 +0300

    COMPLEXCHISLOK15-1

---
+++ KomplexChislo/KomplexChislo/Komplex.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KomplexChislo
+{
+    class Komplex
+    {
+        public decimal a, b;
+
+        public Komplex(decimal real=1, decimal img=3)
+        {
+            a = real; b = img;
+        }
+
+        public override string ToString()
+        {
+            if (b < 0)
+            {
+                return String.Format("{0}{1}i", a, b);
+            }
+            return String.Format("{0}+{1}i", a, b);
+        }
+
+        public static Komplex operator +(Komplex l, Komplex r)
+        {
+            return new Komplex(l.a + r.a, l.b + r.b);
+        }
+
+        public static Komplex operator -(Komplex l, Komplex r)
+        {
+            return new Komplex(l.a - r.a, l.b - r.b);
+        }
+
+        public static Komplex operator *(Komplex l, Komplex r)
+        {
+            return new Komplex(l.a * r.a - l.b * r.b, l.b * r.a + l.a * r.b);
+        }
+
+
+        public static Komplex operator /(Komplex l, Komplex r)
+        {
+            return new Komplex(((l.a *r.a)+(l.b *r.b))/((r.a*r.a)+(r.b*r.b)), ((r.a *l.b)-(r.b *l.a))/((r.a*r.a)+(r.b*r.b)));
+        }
+    }
+
+}
---
+++ KomplexChislo/KomplexChislo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace KomplexChislo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите первое комплексное число через энтер");
+
+            Komplex a = new Komplex(decimal.Parse(Console.ReadLine()), decimal.Parse(Console.ReadLine()));
+            Komplex b = new Komplex(2, 5);
+           
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+
+
+            Console.ReadKey(); 
+        }
+    }
+}
